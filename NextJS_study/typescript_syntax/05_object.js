2cb9df0f5173d9ac91f09b673234055aeda6534a
Jiwon <58460386+isdiscodead@users.noreply.github.com>
2024-03-30 03:43:47 +0900

    ts object

--- NextJS_study/typescript_syntax/05_object.js
+++ NextJS_study/typescript_syntax/05_object.js
@@ -1,24 +1,17 @@
 // 오브젝트 Object
 // OOP에서의 오브젝트와 달리 dict, map과 유사한 개념
 // 여러 변수 혹은 함수가 하나로 구조화 된 형태
-
 var emptyObject = {};
-
 // 안에 있는 변수 하나 하나 = member or property
 var personObject = {
     firstName: 'John',
     lastName: 'Smith'
-}
+};
 personObject['salary'] = 14000;
-
 // check properties
 for (var member in personObject) {
     if (personObject.hasOwnProperty(member)) {
-        console.log('the member ' + member + 
-        ' of personObject is ' + personObject[member]);
+        console.log('the member ' + member +
+            ' of personObject is ' + personObject[member]);
     }
 }
-
-// the member firstName of personObject is John
-// the member lastName of personObject is Smith
-// the member salary of personObject is 14000
