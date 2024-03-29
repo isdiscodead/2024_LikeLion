f2d9945faf630dc6812fc1f1b14263032af3a5d9
Jiwon <58460386+isdiscodead@users.noreply.github.com>
2024-03-30 03:52:19 +0900

    ts interface

---
+++ NextJS_study/typescript_syntax/06_interface.js
@@ -0,0 +1,24 @@
+// Interface 인터페이스 = Object에 대한 타입
+// 여러 개의 함수와 변수 등이 구조적으로 어떻게 결합되어야 하는지에 대한 약속
+// 하나의 함수가 가져야 할 구조에 대한 약속
+var person = {
+    name: 'John',
+    move: function () { }
+};
+var person2 = {
+    name: 'John',
+    age: 24,
+    // move() 없앨 경우 ...
+    // 'move' 속성이 '{ name: string; age: number; }' 형식에 없지만 
+    // 'Person' 형식에서 필수입니다.
+    move: function () { }
+};
+var person3 = {
+    name: 'John',
+    // age: true, -> 'boolean' 형식은 'number' 형식에 할당할 수 없습니다.
+    move: function () { },
+};
+var mySearch; // name is not important
+mySearch = function (src, sub) {
+    return src.search(sub) != -1; // search()는 못 찾으면 -1 반환됨
+};
